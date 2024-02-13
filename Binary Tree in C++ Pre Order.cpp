60a0eed0f86b909ea4fe4ee8cc23b445a0c34ddc
Thanneruk <157442478+Thanneruk@users.noreply.github.com>
2024-02-13 14:21:31 -0500

    Add files via upload

---
+++ Binary Tree in C++ Pre Order.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+void printPreorder(Node* root) {
+    if (root == nullptr)
+        return;
+    cout << root->data << " ";
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
+
+int main() {
+    Node* root = createNode(1);
+    root->left = createNode(2);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->left->right->left = createNode(9);
+    root->right = createNode(3);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+    root->right->right->left = createNode(15);
+
+
+
+
+
+    cout << "Preorder traversal of the tree: ";
+    printPreorder(root);
+    cout << endl;
+
+    // Remember to free the memory allocated for nodes
+    delete root->left->left;
+    delete root->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
